ab1b546d021b090eb117c3b94e258d7fa0c60ebc
Saydulayev <saydulayev.wien@gmail.com>
2023-08-29 12:50:14 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dokx Dig on 28.08.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
